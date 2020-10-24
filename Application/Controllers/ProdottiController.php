07be51133200eede3813d0f9f42c7357fdf66279
antonio <antoniog.web@gmail.com>
2020-10-24 13:11:47 +0200

    fix campi nascosti per ecommerce

--- Application/Controllers/ProdottiController.php
+++ Application/Controllers/ProdottiController.php
@@ -56,7 +56,7 @@ class ProdottiController extends PagesController {
 		$this->queryFields = "title,alias,id_c,attivo,in_evidenza,immagine,sottotitolo";
 		
 		if (v("ecommerce_attivo"))
-			$this->queryFields .= "price,id_iva,codice,peso,in_promozione,prezzo_promozione,dal,al";
+			$this->queryFields .= ",price,id_iva,codice,peso,in_promozione,prezzo_promozione,dal,al";
 		
 		if (v("usa_marchi"))
 			$this->queryFields .= ",id_marchio";
