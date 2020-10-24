288b53ce8b51f5fe827efe3f4111d8d19788475a
antonio <antoniog.web@gmail.com>
2020-10-24 20:21:13 +0200

    fix sql

--- DB/Migrazioni/0096.sql
+++ DB/Migrazioni/0096.sql
@@ -3,9 +3,9 @@ create table reggroups_documenti (
 	id_doc int(11) UNSIGNED not null,
 	id_group int(11) UNSIGNED not null,
 	id_order INT UNSIGNED NOT NULL,
-	index group_indx(id_group),
-	index cont_indx(id_doc),
-	foreign key group_fky(id_group) references reggroups (id_group),
+	index doc_group_indx(id_group),
+	index doc_indx(id_doc),
+	foreign key doc_group_fky(id_group) references reggroups (id_group),
 	foreign key doc_fky(id_doc) references documenti (id_doc),
 	unique (id_group,id_doc)
 )engine=innodb;
