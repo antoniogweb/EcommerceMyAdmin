b09f04bec32dbd4b9a0f08913d65e124427cb0d7
antonio <antoniog.web@gmail.com>
2021-02-13 12:54:54 +0100

    lingue contenuti

--- Application/Views/Contenuti/steps.php
+++ Application/Views/Contenuti/steps.php
@@ -8,7 +8,10 @@
 	<li <?php echo $posizioni['gruppi'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/gruppi/$id".$this->viewStatus;?>">Accessi</a></li>
 	<?php } ?>
 	<?php $traduz = ContenutitradottiModel::getTraduzioni("id_cont", $id);?>
-	<?php foreach ($traduz as $tr) { ?>
+	<?php foreach ($traduz as $tr) {
+		if (!in_array($tr["lingua"], BaseController::$traduzioni))
+			continue;
+	?>
 	<li <?php if (isset($id_ct) && (int)$tr["id_ct"] === (int)$id_ct) { ?>class="active"<?php } ?>><a href="<?php echo $this->baseUrl."/contenuti/traduzione/$id/".$tr["id_ct"].$this->viewStatus;?>"><?php echo strtoupper($tr["lingua"]);?></a></li>
 	<?php } ?>
 </ul>
