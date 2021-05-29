311ae2380ce8f4d313a5cfab48f232f48cf9808a
antonio <antoniog.web@gmail.com>
2021-05-29 14:10:56 +0200

    gestione categorie

--- Application/Views/Categories/categories_steps.php
+++ Application/Views/Categories/categories_steps.php
@@ -4,10 +4,7 @@
 
 <ul class="nav_dettaglio nav nav-tabs">
   <li <?php echo $posizioni['main'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/form/update/$id".$this->viewStatus;?>">Dettagli</a></li>
-	<li <?php echo $posizioni['contenuti'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/contenuti/$id".$this->viewStatus;?>">Contenuti</a></li>
   <li <?php echo $posizioni['meta'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/meta/$id".$this->viewStatus;?>">Meta</a></li>
-   <li <?php echo $posizioni['classisconto'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/classisconto/$id".$this->viewStatus;?>">Classi sconto applicate</a></li>
-<!--   <li <?php echo $posizioni['gruppi'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/gruppi/$id".$this->viewStatus;?>">Accessibilità</a></li> -->
 </ul>
 
 
