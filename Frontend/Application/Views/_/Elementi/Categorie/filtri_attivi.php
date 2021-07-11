529b3c9de50f9463fb830ce8e62b8745c5b319b1
antonio <antoniog.web@gmail.com>
2021-07-11 12:10:30 +0200

    filtro categoria

--- Frontend/Application/Views/_/Elementi/Categorie/filtri_attivi.php
+++ Frontend/Application/Views/_/Elementi/Categorie/filtri_attivi.php
@@ -1,15 +1,24 @@
 <?php if (!defined('EG')) die('Direct access not allowed!');
 
+$viewStatusTogliFiltro = $this->getViewStatusUsingVariables(array("p"=>1));
+
+if ($id_categoria && $id_categoria != $idShop)
+{
+	$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($id_categoria, $idMarchio, $idShop, $viewStatusTogliFiltro, $filtriUrlTuttiAltri, $filtriUrlLocTuttiAltri,$filtriUrlAltriTuttiAltri);
+	$carV = cfield($datiCategoria, "alias");
+	include(tpf("/Elementi/Categorie/filtro_attivo.php"));
+}
+
 if ($idTag && isset($aliasTagCorrente))
 {
-	$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio(0, $idMarchio, $id_categoria, "", $filtriUrlTuttiAltri, $filtriUrlLocTuttiAltri,$filtriUrlAltriTuttiAltri);
+	$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio(0, $idMarchio, $id_categoria, $viewStatusTogliFiltro, $filtriUrlTuttiAltri, $filtriUrlLocTuttiAltri,$filtriUrlAltriTuttiAltri);
 	$carV = $aliasTagCorrente;
 	include(tpf("/Elementi/Categorie/filtro_attivo.php"));
 }
 
 if ($idMarchio && isset($aliasMarchioCorrente))
 {
-	$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, 0, $id_categoria, "", $filtriUrlTuttiAltri, $filtriUrlLocTuttiAltri,$filtriUrlAltriTuttiAltri);
+	$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, 0, $id_categoria, $viewStatusTogliFiltro, $filtriUrlTuttiAltri, $filtriUrlLocTuttiAltri,$filtriUrlAltriTuttiAltri);
 	$carV = $aliasMarchioCorrente;
 	include(tpf("/Elementi/Categorie/filtro_attivo.php"));
 }
@@ -17,7 +26,7 @@ if ($idMarchio && isset($aliasMarchioCorrente))
 foreach (CaratteristicheModel::$filtriUrl as $car => $carVs) {
 	foreach ($carVs as $carV) {
 		$filtroSelezionatoUrlTutti = CaratteristicheModel::getUrlCaratteristicheTutti($car);
-		$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, $idMarchio, $id_categoria, "", $filtroSelezionatoUrlTutti, $filtriUrlLocTuttiAltri,$filtriUrlAltriTuttiAltri);
+		$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, $idMarchio, $id_categoria, $viewStatusTogliFiltro, $filtroSelezionatoUrlTutti, $filtriUrlLocTuttiAltri,$filtriUrlAltriTuttiAltri);
 		include(tpf("/Elementi/Categorie/filtro_attivo.php"));
 	}
 }
@@ -25,13 +34,13 @@ foreach (CaratteristicheModel::$filtriUrl as $car => $carVs) {
 foreach (RegioniModel::$filtriUrl as $car => $carVs) {
 	foreach ($carVs as $carV) {
 		$filtroSelezionatoUrlTutti = RegioniModel::getUrlCaratteristicheTutti($car);
-		$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, $idMarchio, $id_categoria, "", $filtriUrlTuttiAltri, $filtroSelezionatoUrlTutti,$filtriUrlAltriTuttiAltri);
+		$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, $idMarchio, $id_categoria, $viewStatusTogliFiltro, $filtriUrlTuttiAltri, $filtroSelezionatoUrlTutti,$filtriUrlAltriTuttiAltri);
 		include(tpf("/Elementi/Categorie/filtro_attivo.php"));
 	}
 }
 
 foreach (Filtri::$filtriUrl as $car => $carV) {
 	$filtroSelezionatoUrlTutti = Filtri::getUrlCaratteristicheTutti($car);
-	$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, $idMarchio, $id_categoria, "", $filtriUrlTuttiAltri, $filtriUrlLocTuttiAltri,$filtroSelezionatoUrlTutti);
+	$filtroSelezionatoUrl = $this->baseUrl."/".CategoriesModel::getUrlAliasTagMarchio($idTag, $idMarchio, $id_categoria, $viewStatusTogliFiltro, $filtriUrlTuttiAltri, $filtriUrlLocTuttiAltri,$filtroSelezionatoUrlTutti);
 	include(tpf("/Elementi/Categorie/filtro_attivo.php"));
 }
