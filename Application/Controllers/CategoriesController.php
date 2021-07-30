e12cfcb55250887c3fcd43f9c2c5d2027f62eafa
antonio <antoniog.web@gmail.com>
2021-07-31 01:40:11 +0200

    no parent su gestione sezioni

--- Application/Controllers/CategoriesController.php
+++ Application/Controllers/CategoriesController.php
@@ -423,6 +423,9 @@ class CategoriesController extends BaseController {
 		
 			$data['type'] = $queryType;
 			
+			if (!$this->m[$this->modelName]->section)
+				$this->queryFields = "title,alias,sottotitolo,immagine,description";
+			
 			$this->m[$this->modelName]->setFields($this->queryFields,'sanitizeAll');
 			
 			$data["section"] = $this->m[$this->modelName]->section;
