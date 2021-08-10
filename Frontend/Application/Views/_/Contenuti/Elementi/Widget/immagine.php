fb2f3148b1dcd0390c9da3801d61edfe78e430bf
antonio <antoniog.web@gmail.com>
2021-08-10 15:29:20 +0200

    widget immagine

--- Frontend/Application/Views/_/Contenuti/Elementi/Widget/immagine.php
+++ Frontend/Application/Views/_/Contenuti/Elementi/Widget/immagine.php
@@ -6,9 +6,7 @@
 		<?php $ext = Files_Upload::sFileExtension($testo["immagine"]);?>
 		<?php if ($ext != "svg") { ?>
 			<?php if ($testo["immagine_2x"]) { ?>
-				<img uk-img data-srcset="
-				<?php echo Domain::$publicUrl."/thumb/widget/".$testo["id_t"]."/".$testo["immagine"];?> 1x, 
-				<?php echo Domain::$publicUrl."/thumb/widget2x/".$testo["id_t"]."/".$testo["immagine_2x"];?> 2x" />
+				<img uk-img data-srcset="<?php echo Domain::$publicUrl."/thumb/widget/".$testo["id_t"]."/".$testo["immagine"];?> 1x, <?php echo Domain::$publicUrl."/thumb/widget2x/".$testo["id_t"]."/".$testo["immagine_2x"];?> 2x" srcset="<?php echo Domain::$publicUrl."/thumb/widget/".$testo["id_t"]."/".$testo["immagine"];?> 1x, <?php echo Domain::$publicUrl."/thumb/widget2x/".$testo["id_t"]."/".$testo["immagine_2x"];?> 2x"/>
 			<?php } else { ?>
 				<img <?php echo htmlentitydecode($testo["attributi"]);?> src="<?php echo Domain::$publicUrl."/thumb/widget/".$testo["id_t"]."/".$testo["immagine"];?>" />
 			<?php } ?>
