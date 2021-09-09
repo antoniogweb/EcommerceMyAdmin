b29b26aae4b98a4b62e8776e2df9d1431d1c5919
antonio <antoniog.web@gmail.com>
2021-09-09 17:47:33 +0200

    guida

--- Application/Views/Categorie/categories_steps.php
+++ Application/Views/Categorie/categories_steps.php
@@ -7,8 +7,8 @@
 	<?php if (v("fasce_in_categorie")) { ?>
 		<li <?php echo $posizioni['contenuti'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/contenuti/$id".$this->viewStatus;?>">Fasce</a></li>
 	<?php } ?>
-	<li <?php echo $posizioni['meta'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/meta/$id".$this->viewStatus;?>">Meta</a></li>
-	<?php if (v("ecommerce_attivo")) { ?>
+	<li <?php echo $posizioni['meta'];?>><a class="help_meta" href="<?php echo $this->baseUrl."/".$this->controller."/meta/$id".$this->viewStatus;?>">Meta</a></li>
+	<?php if (v("ecommerce_attivo") && v("attiva_classi_sconto")) { ?>
 		<li <?php echo $posizioni['classisconto'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/classisconto/$id".$this->viewStatus;?>">Classi sconto applicate</a></li>
 	<?php } ?>
 	<!--   <li <?php echo $posizioni['gruppi'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/gruppi/$id".$this->viewStatus;?>">Accessibilità</a></li> -->
