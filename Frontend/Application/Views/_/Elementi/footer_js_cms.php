c5c94f9a7e4ebcca5c63ba25cc63990c9a7f986d
antonio <antoniog.web@gmail.com>
2021-09-17 15:46:36 +0200

    uikit footer

--- Frontend/Application/Views/_/Elementi/footer_js_cms.php
+++ Frontend/Application/Views/_/Elementi/footer_js_cms.php
@@ -4,5 +4,7 @@
 <script src="<?php echo $this->baseUrlSrc.'/admin/Frontend/Public/Js/';?>functions.js?v=<?php echo rand(1,10000);?>"></script>
 <script src="<?php echo $this->baseUrlSrc.'/admin/Frontend/Public/Js/';?>cart.js?v=<?php echo rand(1,10000);?>"></script>
 
+<script src="<?php echo $this->baseUrlSrc."/admin/Frontend/Public/Js/uikit/"?>uikit-icons.min.js"></script>
+
 <script type='text/javascript' src='<?php echo $this->baseUrlSrc;?>/admin/Frontend/Public/Js/icheck.min.js'></script>
 <script type='text/javascript' src='<?php echo $this->baseUrlSrc;?>/admin/Frontend/Public/Js/image-picker/image-picker.min.js'></script>
