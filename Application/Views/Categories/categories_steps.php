c06c8b7d176428759da6f48107f3d1df52ce63e5
antonio <antoniog.web@gmail.com>
2021-10-01 11:16:04 +0200

    fasce

--- Application/Views/Categories/categories_steps.php
+++ Application/Views/Categories/categories_steps.php
@@ -3,8 +3,11 @@
 <?php if ($type !== "insert") { ?>
 
 <ul class="nav_dettaglio nav nav-tabs">
-  <li <?php echo $posizioni['main'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/form/update/$id".$this->viewStatus;?>">Dettagli</a></li>
-  <li <?php echo $posizioni['meta'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/meta/$id".$this->viewStatus;?>">Meta</a></li>
+	<li <?php echo $posizioni['main'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/form/update/$id".$this->viewStatus;?>"><?php echo gtext("Dettagli");?></a></li>
+	<?php if (v("fasce_in_categorie")) { ?>
+		<li <?php echo $posizioni['contenuti'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/contenuti/$id".$this->viewStatus;?>"><?php echo gtext("Fasce");?></a></li>
+	<?php } ?>
+	<li <?php echo $posizioni['meta'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/meta/$id".$this->viewStatus;?>"><?php echo gtext("Meta");?></a></li>
 </ul>
 
 <?php } ?>
