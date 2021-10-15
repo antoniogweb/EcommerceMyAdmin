6fe5112b11eaf322ac76c338466ba7d2f305e773
antonio <antoniog.web@gmail.com>
2021-10-15 12:04:37 +0200

    antispam

--- Frontend/Application/Views/_/Elementi/Captcha/campo-nascosto-registrazione.php
+++ Frontend/Application/Views/_/Elementi/Captcha/campo-nascosto-registrazione.php
@@ -2,7 +2,7 @@
 <?php
 $params = CaptchaModel::getModulo()->getParams();
 ?>
-<div class="uk-margin uk-margin-remove-bottom">
+<div class="uk-margin uk-margin-remove-bottom t">
 	<label class="uk-form-label"><?php echo ucfirst($params["campo_nascosto_registrazione"]);?></label>
 	<div class="uk-form-controls">
 		<?php echo Html_Form::input($params["campo_nascosto_registrazione"], "", "uk-input ".$params["campo_nascosto_registrazione"], null, "placeholder='".ucfirst(gtext($params["campo_nascosto_registrazione"]."*"))."'");?>
