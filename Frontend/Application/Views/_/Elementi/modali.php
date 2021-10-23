aa10303b17e5c0e080d554541569b773545ad782
antonio <antoniog.web@gmail.com>
2021-10-23 19:21:38 +0200

    load modali

--- Frontend/Application/Views/_/Elementi/modali.php
+++ Frontend/Application/Views/_/Elementi/modali.php
@@ -1,7 +1,13 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 <?php if (isset($modali_frontend)) { ?>
 	<?php foreach ($modali_frontend as $p) { ?>
-		<?php include(tpf("/Elementi/Modali/modale_stile_1.php"));?>
+		<?php 
+		$pathModale = tpf("/Elementi/Modali/".$p["pages"]["template_modale"]);
+		
+		if (!file_exists($pathModale))
+			continue;
+		
+		include($pathModale);?>
 		
 		<?php if (!isset($_COOKIE["modale_".$p["pages"]["id_page"]])) { ?>
 			<script>
