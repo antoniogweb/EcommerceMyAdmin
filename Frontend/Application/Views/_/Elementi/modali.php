b80a340e388259e539a07ecbb20b4da4d6822842
antonio <antoniog.web@gmail.com>
2021-10-30 22:10:50 +0200

    modali

--- Frontend/Application/Views/_/Elementi/modali.php
+++ Frontend/Application/Views/_/Elementi/modali.php
@@ -1,15 +1,15 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 <?php if (isset($modali_frontend)) { ?>
 	<?php foreach ($modali_frontend as $p) { ?>
-		<?php 
-		$pathModale = tpf("/Elementi/Modali/".$p["pages"]["template_modale"]);
-		
-		if (!file_exists($pathModale))
-			continue;
-		
-		include($pathModale);?>
-		
 		<?php if (!isset($_COOKIE["modale_".$p["pages"]["id_page"]])) { ?>
+			<?php 
+			$pathModale = tpf("/Elementi/Modali/".$p["pages"]["template_modale"]);
+			
+			if (!file_exists($pathModale))
+				continue;
+			
+			include($pathModale);?>
+		
 			<script>
 			setTimeout(function(){
 				UIkit.modal("#modale_<?php echo $p["pages"]["id_page"];?>").show();
