ebb46d6b2ffa658f72aedeba8774c8e6f0f525ea
antonio <antoniog.web@gmail.com>
2021-11-02 12:11:21 +0100

    cookie

--- Frontend/Application/Views/_/Elementi/Cookie/scelta_tipo.php
+++ Frontend/Application/Views/_/Elementi/Cookie/scelta_tipo.php
@@ -2,5 +2,5 @@
 
 <div class="uk-margin">
 	<a class="uk-margin uk-button uk-button-primary uk-width-1-3@m" title="<?php echo gtext("accetto", false);?>" href="<?php echo Domain::$name."/accept-cookies?all_cookie&".v("var_query_string_no_cookie");?>"><?php echo gtext("Accetta tutti i cookie");?></a>
-	<a style="float:right;" class="uk-button uk-button-default uk-width-1-3@m" title="<?php echo gtext("accetto", false);?>" href="<?php echo Domain::$name."/accept-cookies?".v("var_query_string_no_cookie");?>"><?php echo gtext("Accetta solo i cookie tecnici");?></a>
+	<a style="float:right;" class="cookie_personalizza uk-button uk-button-default uk-width-1-3@m" title="<?php echo gtext("accetto", false);?>" href="<?php echo Domain::$name."/accept-cookies?".v("var_query_string_no_cookie");?>"><?php echo gtext("Accetta solo i cookie tecnici");?></a>
 </div>
