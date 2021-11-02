d5bee732595f1afe6b2780e389827cb3dd543236
antonio <antoniog.web@gmail.com>
2021-11-02 12:06:59 +0100

    cookie

--- Frontend/Application/Views/_/Elementi/Cookie/Varianti/accetta_approfondisci.php
+++ Frontend/Application/Views/_/Elementi/Cookie/Varianti/accetta_approfondisci.php
@@ -6,13 +6,13 @@
 <a class="" href="<?php echo $this->baseUrl."/".getUrlAlias($tipiPagina["COOKIE"]);?>"><?php echo gtext("Ulteriori informazioni");?></a>
 <?php } ?></p>
 
-<div class="uk-margin uk-container <?php if (!User::$isPhone) { ?>uk-container-small uk-flex uk-flex-around<?php } ?>">
-	<a class="uk-margin-top uk-width-1-1 uk-width-1-3@s ok_cookies cookie_accetta uk-button uk-button-primary" title="<?php echo gtext("accetto", false);?>" href="#">
+<div class="uk-margin <?php if (v("stile_popup_cookie") != "cookie_stile_modale") { ?>uk-container uk-container-small<?php } ?> <?php if (!User::$isPhone) { ?>uk-flex uk-flex-between<?php } ?>">
+	<a class="uk-margin-top uk-width-1-1 uk-width-2-5@s ok_cookies cookie_accetta uk-button uk-button-primary" title="<?php echo gtext("accetto", false);?>" href="#">
 		<span uk-icon="icon: check"></span>
 		<?php echo gtext("Accetta");?>
 	</a>
 	<?php if (isset($tipiPagina["COOKIE"])) { ?>
-	<a style="" class="cookie_personalizza uk-margin-top uk-width-1-1 uk-width-1-3@s uk-button uk-button-default" title="<?php echo gtext("personalizza", false);?>" href="<?php echo $this->baseUrl."/".getUrlAlias($tipiPagina["COOKIE"])."?".v("var_query_string_no_cookie");?>">
+	<a style="" class="cookie_personalizza uk-margin-top uk-width-1-1 uk-width-2-5@s uk-button uk-button-default" title="<?php echo gtext("personalizza", false);?>" href="<?php echo $this->baseUrl."/".getUrlAlias($tipiPagina["COOKIE"])."?".v("var_query_string_no_cookie");?>">
 		<span uk-icon="icon: cog"></span>
 		<?php echo gtext("Personalizza");?>
 	</a>
