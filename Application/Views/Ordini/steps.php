1f4dfa51cbb3d66ac576fe67d948ccf9d0a709e6
antonio <antoniog.web@gmail.com>
2021-11-30 17:11:54 +0100

    integrazioni

--- Application/Views/Ordini/steps.php
+++ Application/Views/Ordini/steps.php
@@ -1,8 +1,10 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 
+<?php if (IntegrazioniModel::getElencoIntegrazioni("ORDINE")) { ?>
 <ul class="nav_dettaglio nav nav-tabs">
 	<li <?php echo $posizioni['main'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/vedi/$id".$this->viewStatus;?>">Dettagli</a></li>
 	<li <?php echo $posizioni['integrazioni'];?>><a href="<?php echo $this->baseUrl."/".$this->controller."/integrazioni/$id".$this->viewStatus;?>">Invii a piattaforme esterne</a></li>
 </ul>
 
 <div style="clear:left;"></div>
+<?php } ?>
