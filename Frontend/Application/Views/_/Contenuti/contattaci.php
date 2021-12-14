2723db0c3d3713161717a1ec88ec10e1ae3ac15d
antonio <antoniog.web@gmail.com>
2021-12-14 19:19:02 +0100

    fasce

--- Frontend/Application/Views/_/Contenuti/contattaci.php
+++ Frontend/Application/Views/_/Contenuti/contattaci.php
@@ -1,2 +1,26 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
-<?php include(tpf("/Contenuti/page.php"));?>
+<?php
+
+$standardPage = false;
+$noNumeroProdotti = $noContainer = $noContainer = true;
+
+foreach ($pages as $p) {
+	$datiCategoria = $p;
+	$urlAlias = getUrlAlias($p["pages"]["id_page"]);
+	$urlAliasCategoria = getCategoryUrlAlias($p["categories"]["id_c"]);
+
+	$titoloPagina = field($p, "title");
+	$sottotitoloPagina = field($p, "sottotitolo");
+	
+	if (!isset($noTopPagina))
+		include(tpf("/Elementi/Pagine/page_top.php"));
+	
+	include(tpf(ElementitemaModel::p("CONTATTI_TOP","", array(
+		"titolo"	=>	"Parte superiore contatti",
+		"percorso"	=>	"Elementi/Sezioni/Pagine/Contatti/Top",
+	))));
+
+	echo $fasce;
+
+	include(tpf("/Elementi/Pagine/page_bottom.php"));
+}
