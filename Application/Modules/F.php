b9bd92b7a1809f733b4793e68f00dd25b4f91609
antonio <antoniog.web@gmail.com>
2021-12-18 12:22:08 +0100

    modulo F

---
+++ Application/Modules/F.php
@@ -0,0 +1,40 @@
+<?php
+
+// EcommerceMyAdmin is a PHP CMS based on MvcMyLibrary
+//
+// Copyright (C) 2009 - 2020  Antonio Gallo (info@laboratoriolibero.com)
+// See COPYRIGHT.txt and LICENSE.txt.
+//
+// This file is part of EcommerceMyAdmin
+//
+// EcommerceMyAdmin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// EcommerceMyAdmin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with EcommerceMyAdmin.  If not, see <http://www.gnu.org/licenses/>.
+
+if (!defined('EG')) die('Direct access not allowed!');
+
+class F
+{
+	public static function mediana($arr) {
+		sort($arr);
+		$count = count($arr);
+		$middleval = floor(($count-1)/2);
+		if ($count % 2) {
+			$median = $arr[$middleval];
+		} else {
+			$low = $arr[$middleval];
+			$high = $arr[$middleval+1];
+			$median = (($low+$high)/2);
+		}
+		return $median;
+	}
+}
