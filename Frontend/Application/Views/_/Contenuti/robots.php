4e01c9c6c7e8e76187b95ae43cae059e2ac73928
antonio <antoniog.web@gmail.com>
2022-01-09 00:11:30 +0100

    robots.txt

--- Frontend/Application/Views/_/Contenuti/robots.php
+++ Frontend/Application/Views/_/Contenuti/robots.php
@@ -1,5 +1,5 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 User-agent: *
 Disallow:<?php if (v("piattaforma_in_sviluppo")) { echo " / "; } echo "\n";?>
-<?php if (!v("piattaforma_in_sviluppo")) { echo "Allow: /"; } echo "\n";?>
+<?php /*if (!v("piattaforma_in_sviluppo")) { echo "Allow: /"; } echo "\n";*/?>
 Sitemap: <?php echo $this->baseUrlSrc;?>/sitemap.xml 
