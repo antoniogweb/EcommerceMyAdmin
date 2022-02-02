0673617b4aca6859aa9f9acc91c490353c78b8e1
antonio <antoniog.web@gmail.com>
2022-02-02 15:58:41 +0100

    mail template

--- Frontend/Application/Views/_/Elementi/Mail/mail_approvazione_gruppi_utente.php
+++ Frontend/Application/Views/_/Elementi/Mail/mail_approvazione_gruppi_utente.php
@@ -5,7 +5,7 @@
 	<?php echo gtext("il suo account è stato attivato.")?>
 </p>
 
-<p><?php echo gtext("Potrà accedere alla propria area riservata tramite username e password che ha scelto e visitando il seguente",false);?> <a href="<?php echo Url::getRoot()."area-riservata";?>"><?php echo gtext("indirizzo web",false);?></a>.
+<p><?php echo gtext("Potrà accedere alla propria area riservata tramite username e password che ha scelto e visitando il seguente",false);?> <a href="<?php echo Domain::$publicUrl."/area-riservata";?>"><?php echo gtext("indirizzo web",false);?></a>.
 </p>
 
 <p><?php echo gtext("Cordiali saluti", false);?>.</p>
