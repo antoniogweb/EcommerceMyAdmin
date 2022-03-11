8694b90a2786ee9bceb660ac3ca8500fcafbcdbc
antonio <antoniog.web@gmail.com>
2022-03-11 09:25:01 +0100

    cookie

--- Frontend/Application/Views/_/Elementi/Cookie/testo_popup_cookies.php
+++ Frontend/Application/Views/_/Elementi/Cookie/testo_popup_cookies.php
@@ -1,6 +1,6 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 
-<div class="uk-margin <?php if (v("stile_popup_cookie") == "cookie_stile_css") { ?>uk-container<?php } ?> uk-text-justify">
+<div style="max-height:200px;overflow:auto;" class="uk-margin <?php if (v("stile_popup_cookie") == "cookie_stile_css") { ?>uk-container<?php } ?> uk-text-justify">
 	<?php echo gtext("Questo sito utilizza cookie tecnici e funzionali per migliorare la tua esperienza di navigazione.");?> 
 	<?php if (VariabiliModel::$usatiCookieTerzi) { ?>
 	<?php echo gtext("Con il tuo consenso vorremmo attivare cookie con finalità di analisi e di marketing, allo scopo di migliorare la tua esperienza di navigazione e di mostrarti prodotti e servizi di tuo interesse.");?> <?php echo gtext("Puoi modificare le tue impostazioni in qualsiasi momento nella pagina delle condizioni sui cookie."); ?>
