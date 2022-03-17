bb9d8c2c3c1d472e787a5f2b973ce02108606f53
antonio <antoniog.web@gmail.com>
2022-03-17 16:55:37 +0100

    notice

--- Frontend/Application/Views/_/Elementi/Registrazione/Resoconto/account_creato.php
+++ Frontend/Application/Views/_/Elementi/Registrazione/Resoconto/account_creato.php
@@ -2,4 +2,4 @@
 
 <p><?php echo gtext("L'account è stato creato correttamente. Le è stata inviata una mail con le credenziali d'accesso che ha scelto");?>.</p>
 
-<p><?php echo gtext("Vai all'");?> <a href="<?php echo $this->baseUrl."/area-riservata";?>"><?php echo strtolower(gtext("Area riservata"));?></a></p>
+<p class="uk-margin-large"><a class="uk-button uk-button-default" href="<?php echo $this->baseUrl."/area-riservata";?>"><?php echo gtext("Procedi nell' area riservata");?><span uk-icon="arrow-right"></span></a></p>
