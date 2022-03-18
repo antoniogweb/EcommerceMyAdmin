a0df31790a1ee49b94a0608a83adf179048bb05b
antonio <antoniog.web@gmail.com>
2022-03-18 11:05:06 +0100

    cookie

--- Frontend/Application/Views/_/Elementi/Cookie/link_pagina_info_privacy.php
+++ Frontend/Application/Views/_/Elementi/Cookie/link_pagina_info_privacy.php
@@ -3,6 +3,7 @@
 <?php if (isset(PagesModel::$tipiPaginaId["PRIVACY"])) { ?>
 	<?php if (v("informativa_privacy_in_pagina_cookie")) {?>
 		<?php $paginaPrivacy = PagesModel::getPageDetails(PagesModel::$tipiPaginaId["PRIVACY"]);?>
+		<h3><?php echo htmlentitydecode(field($paginaPrivacy, "title"));?></h3>
 		<?php echo htmlentitydecode(attivaModuli(field($paginaPrivacy, "description")));?>
 	<?php } else { ?>
 		<h3><?php echo gtext("Informativa sul trattamento dei dati");?></h3>
