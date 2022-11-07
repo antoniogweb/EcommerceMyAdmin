19979eb6c14c745aebcc657603b0cd80176cb320
antonio <antoniog.web@gmail.com>
2022-11-07 19:07:54 +0100

    checkout

--- Frontend/Application/Views/_/Ordini/totali.php
+++ Frontend/Application/Views/_/Ordini/totali.php
@@ -9,7 +9,7 @@ if ($haCouponAttivo)
 <?php if (v("attiva_spedizione") || $haCouponAttivo || $costoPagamento > 0) { ?>
 <div class="uk-grid-small uk-grid" uk-grid="">
 	<div class="uk-width-expand uk-text-muted uk-first-column"><?php echo gtext("Totale merce");?></div>
-	<div><?php echo getSubTotal(v("prezzi_ivati_in_carrello"));?> €</div>
+	<div class="uk-margin-remove-top"><?php echo getSubTotal(v("prezzi_ivati_in_carrello"));?> €</div>
 </div>
 <?php } ?>
 <?php if ($haCouponAttivo && $couponAttivo["tipo_sconto"] == "PERCENTUALE") { ?>
@@ -18,25 +18,25 @@ if ($haCouponAttivo)
 		<?php echo gtext("Totale scontato");?> (<i><?php echo $couponAttivo["titolo"];?></i>)
 		<?php include(tpf("/Ordini/totale_promo_attiva.php"));?>
 	</div>
-	<div><?php echo getPrezzoScontato(v("prezzi_ivati_in_carrello"));?> €</div>
+	<div class="uk-margin-remove-top"><?php echo getPrezzoScontato(v("prezzi_ivati_in_carrello"));?> €</div>
 </div>
 <?php } ?>
 <?php if ($costoPagamento > 0) { ?>
 <div class="uk-grid-small uk-grid" uk-grid="">
 	<div class="uk-width-expand uk-text-muted uk-first-column"><?php echo gtext("Spese pagamento");?></div>
-	<div><?php echo setPriceReverse($costoPagamento);?> €</div>
+	<div class="uk-margin-remove-top"><?php echo setPriceReverse($costoPagamento);?> €</div>
 </div>
 <?php } ?>
 <?php if (v("attiva_spedizione")) { ?>
 <div class="uk-grid-small uk-grid" uk-grid="">
 	<div class="uk-width-expand uk-text-muted uk-first-column"><?php echo gtext("Spese spedizione");?></div>
-	<div><?php echo getSpedizione(v("prezzi_ivati_in_carrello"));?> €</div>
+	<div class="uk-margin-remove-top"><?php echo getSpedizione(v("prezzi_ivati_in_carrello"));?> €</div>
 </div>
 <?php } ?>
 <?php if (!v("prezzi_ivati_in_carrello")) { ?>
 <div class="uk-grid-small uk-grid" uk-grid="">
 	<div class="uk-width-expand uk-text-muted uk-first-column"><?php echo gtext("Iva");?></div>
-	<div><?php echo getIva();?> €</div>
+	<div class="uk-margin-remove-top"><?php echo getIva();?> €</div>
 </div>
 <?php } ?>
 
@@ -57,7 +57,7 @@ if ($haCouponAttivo)
 		<?php echo gtext("Credito utilizzabile");?>: <b><?php echo setPriceReverse(PromozioniModel::gNumeroEuroRimasti($couponAttivo["id_p"]));?> €</b>
 		</div>
 	</div>
-	<div><?php echo setPriceReverse(getTotalN() - getTotalN(true));?> €</div>
+	<div class="uk-margin-remove-top"><?php echo setPriceReverse(getTotalN() - getTotalN(true));?> €</div>
 </div>
 <?php } ?>
 
