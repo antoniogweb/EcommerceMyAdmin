f304d775ba9146864dbd72499363b0ba70682c1c
antonio <antoniog.web@gmail.com>
2022-11-30 15:17:24 +0100

    file robots.txt

--- Frontend/Application/Views/_/Contenuti/robots.php
+++ Frontend/Application/Views/_/Contenuti/robots.php
@@ -2,4 +2,6 @@
 User-agent: *
 Disallow:<?php if (v("piattaforma_in_sviluppo")) { echo " / "; } echo "\n";?>
 <?php /*if (!v("piattaforma_in_sviluppo")) { echo "Allow: /"; } echo "\n";*/?>
+<?php if (!v("piattaforma_in_sviluppo")) { ?>
 Sitemap: <?php echo $this->baseUrlSrc;?>/sitemap.xml
+<?php } ?>
