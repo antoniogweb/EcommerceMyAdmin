00be02b6a59a1b87820586646211d78af670bafd
antonio <antoniog.web@gmail.com>
2022-12-09 10:03:54 +0100

    cart

--- Frontend/Application/Views/_/Cart/main_campi_right.php
+++ Frontend/Application/Views/_/Cart/main_campi_right.php
@@ -26,6 +26,9 @@
 	<div class="uk-visible@m">
 		<?php echo setPriceReverse($prezzoUnitario);?> €
 		<?php if (strcmp($p["cart"]["in_promozione"],"Y")===0){ echo "<del class='uk-text-small uk-text-muted'>".setPriceReverse(p($p["cart"],$p["cart"]["prezzo_intero"]))." €</del>"; } ?>
+		<?php if (!v("prezzi_ivati_in_carrello")) { ?>
+		<div class="uk-text-meta"><?php echo gtext("Iva")?>: <?php echo setPriceReverse($p["cart"]["iva"]);?>%</div>
+		<?php } ?>
 	</div>
 	<div>
 		<?php if (!v("carrello_monoprodotto")) { ?>
