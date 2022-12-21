ddcef1bd4dae4e5c81cd407bbfa50d9251a35440
antonio <antoniog.web@gmail.com>
2022-12-21 18:20:32 +0100

    database layer

--- Application/Controllers/Public/BaseContenutiController.php
+++ Application/Controllers/Public/BaseContenutiController.php
@@ -1335,7 +1335,7 @@ class BaseContenutiController extends BaseController
 			if ($data["tipoPagina"] == "LISTA_REGALO")
 				$this->getAppLogin();
 		}
-// 		var_dump($firstSection);die();
+		
 		$data["paginaPrecedente"] = $this->m['PagesModel']->where(array(
 			"OR"	=>	array(
 				"lt"	=>	array("pages.data_news"	=>	sanitizeDb($data['pages'][0]["pages"]["data_news"])),
