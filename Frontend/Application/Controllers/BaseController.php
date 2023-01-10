73067957268c823dbb0818e841de8aa5f50b7db0
antonio <antoniog.web@gmail.com>
2023-01-10 18:57:54 +0100

    crud frontend

--- Frontend/Application/Controllers/BaseController.php
+++ Frontend/Application/Controllers/BaseController.php
@@ -31,5 +31,5 @@ require_once(LIBRARY."/Application/Controllers/Public/BaseBaseController.php");
 class BaseController extends BaseBaseController
 {
 	use FasceController;
-// 	use CrudController; // copy to your frontend controller directory and remove comment of this line if want CRUD capabilities in frontend 
+// 	use CrudController; // copy the file to your frontend controller directory and remove the comment of this line if want CRUD capabilities in frontend 
 }
