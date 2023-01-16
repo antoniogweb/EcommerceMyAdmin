8a2018b610cd4da0e46a45eb479a9c1c61e12c71
antonio <antoniog.web@gmail.com>
2023-01-16 18:02:30 +0100

    php8.2

--- Application/Controllers/ThumbController.php
+++ Application/Controllers/ThumbController.php
@@ -25,7 +25,9 @@ if (!defined('EG')) die('Direct access not allowed!');
 require_once(LIBRARY."/Application/Controllers/Public/BaseThumbController.php");
 
 class ThumbController extends BaseThumbController {
-
+	
+	public $publicRoot;
+	
 	public function __construct($model, $controller, $queryString) {
 		parent::__construct($model, $controller, $queryString);
 		
