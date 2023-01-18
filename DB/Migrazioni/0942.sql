047ceb3fcd1cd9189456ac46f71463098491b8c2
antonio <antoniog.web@gmail.com>
2023-01-18 17:30:05 +0100

    ip filter

---
+++ DB/Migrazioni/0942.sql
@@ -0,0 +1,1 @@
+INSERT INTO `controllers` (`id_controller`, `titolo`, `codice`, `codice_padre`, `visibile`, `condizioni`, `id_order`, `attivo`, `pannello`, `action_per_link`) VALUES (NULL, 'Gestione filtri IP', 'ipfilter', '', '1', 'attiva_check_ip=1', '88', '1', 'utenti', 'main')
