dc64b7efa5478c64a4d22a7a4295da8fe0ebf91e
antonio <antoniog.web@gmail.com>
2023-02-03 15:39:54 +0100

    aggiorna

--- Application/Commands/aggiorna.sh
+++ Application/Commands/aggiorna.sh
@@ -1,16 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 cd ../../
 
 # aggiorno admin
-echo aggiorno il CMS
+echo 1 - Aggiorno il CMS
 git pull
 
 # aggiorno Library
-echo Aggiorno la libreria
+echo 2 - Aggiorno la libreria
 cd Library
 git pull
 
+# Faccio il dump del DB
+echo 3 - Faccio il dump del DB
+mkdir -p ../Logs/Dumps
+nome_file=`date +%Y-%m-%d_%H:%M:%S_dump.sql`
+
+while read line; do
+	if [[ $line =~ db_name[[:space:]]?\=[[:space:]]?(\'|\")(.*?)(\'|\") ]]; then
+		db_name="${BASH_REMATCH[2]}"
+	fi
+	if [[ $line =~ db_user[[:space:]]?\=[[:space:]]?(\'|\")(.*?)(\'|\") ]]; then
+		db_user="${BASH_REMATCH[2]}"
+	fi
+	if [[ $line =~ db_pwd[[:space:]]?\=[[:space:]]?(\'|\")(.*?)(\'|\") ]]; then
+		db_pwd="${BASH_REMATCH[2]}"
+	fi
+	if [[ $line =~ db_host[[:space:]]?\=[[:space:]]?(\'|\")(.*?)(\'|\") ]]; then
+		db_host="${BASH_REMATCH[2]}"
+	fi
+done < ../config.php
+
+mysqldump -u $db_user -p$db_pwd -h $db_host --no-tablespaces $db_name  > "../Logs/Dumps/$nome_file"
+tar -zcvf "../Logs/Dumps/$nome_file.tar.gz" "../Logs/Dumps/$nome_file" 
+rm "../Logs/Dumps/$nome_file"
+
 # lancio le migrazoni
-echo Aggiorno il database
+echo 4 - Aggiorno il database
 cd ../Application/Commands
 php migrazioni.php
