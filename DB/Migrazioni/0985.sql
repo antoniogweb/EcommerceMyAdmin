7ed5da5a72b95531d741d0a56da7cf8fe26586c8
antonio <antoniog.web@gmail.com>
2023-02-07 20:27:47 +0100

    queries

---
+++ DB/Migrazioni/0985.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `immagini` ADD INDEX `immagini_id_c_id_order` (`id_c`, `id_order`);
---
+++ DB/Migrazioni/0986.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `combinazioni` ADD INDEX `combinazioni_covering_index` (`canonical`, `codice`, `peso`, `id_page`, `price_scontato`, `price_scontato_ivato`);
