c125ea0d1dbc3921c6cd407a5bacac5af0ae973d
antonio <antoniog.web@gmail.com>
2023-03-17 11:03:32 +0100

    social info on meta

--- Frontend/Application/Views/_/Elementi/header_meta_cms.php
+++ Frontend/Application/Views/_/Elementi/header_meta_cms.php
@@ -11,26 +11,4 @@ include(tpf("Elementi/header_rich_snippet.php", false, false, $stringaCache));?>
 <?php echo $tagCanonical;?>
 <?php } ?>
 
-<?php if (isset($isPage)) { ?>
-	<?php foreach ($pages as $p) {
-		$urlAlias = getUrlAlias($p["pages"]["id_page"]);
-	?>
-		<!-- for Facebook -->       
-		<meta property="og:title" content="<?php echo F::meta($p["pages"]["title"],1000);?>" />
-		<meta property="og:type" content="article" />
-		<?php if (strcmp($p["pages"]["immagine"],"") !== 0) { ?>
-		<meta property="og:image" content="<?php echo $this->baseUrlSrc."/thumb/dettaglio/".$p["pages"]["immagine"];?>" />
-		<?php } ?>
-		<meta property="og:url" content="<?php echo $this->baseUrl."/$urlAlias";?>" />
-		<meta property="og:description" content="<?php echo F::meta($p["pages"]["description"],200);?>" />
-		
-		<!-- for Twitter -->          
-		<meta name="twitter:card" content="summary" />
-		<meta name="twitter:title" content="<?php echo F::meta($p["pages"]["title"],1000);?>" />
-		<meta name="twitter:description" content="<?php echo F::meta($p["pages"]["description"],200);?>" />
-		
-		<?php if (strcmp($p["pages"]["immagine"],"") !== 0) { ?>
-		<meta name="twitter:image" content="<?php echo $this->baseUrlSrc."/thumb/dettaglio/".$p["pages"]["immagine"];?>" />
-		<?php } ?>
-	<?php } ?>
-<?php } ?>
+<?php include(tpf("Elementi/header_meta_info_social.php")); ?>
---
+++ Frontend/Application/Views/_/Elementi/header_meta_info_social.php
@@ -0,0 +1,25 @@
+<?php if (!defined('EG')) die('Direct access not allowed!'); ?>
+
+<?php if (isset($isPage)) { ?>
+	<?php foreach ($pages as $p) {
+		$urlAlias = getUrlAlias($p["pages"]["id_page"]);
+	?>
+		<!-- for Facebook -->       
+		<meta property="og:title" content="<?php echo F::meta($p["pages"]["title"],1000);?>" />
+		<meta property="og:type" content="article" />
+		<?php if (strcmp($p["pages"]["immagine"],"") !== 0) { ?>
+		<meta property="og:image" content="<?php echo $this->baseUrlSrc."/thumb/dettaglio/".$p["pages"]["immagine"];?>" />
+		<?php } ?>
+		<meta property="og:url" content="<?php echo $this->baseUrl."/$urlAlias";?>" />
+		<meta property="og:description" content="<?php echo F::meta($p["pages"]["description"],200);?>" />
+		
+		<!-- for Twitter -->          
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:title" content="<?php echo F::meta($p["pages"]["title"],1000);?>" />
+		<meta name="twitter:description" content="<?php echo F::meta($p["pages"]["description"],200);?>" />
+		
+		<?php if (strcmp($p["pages"]["immagine"],"") !== 0) { ?>
+		<meta name="twitter:image" content="<?php echo $this->baseUrlSrc."/thumb/dettaglio/".$p["pages"]["immagine"];?>" />
+		<?php } ?>
+	<?php } ?>
+<?php } ?>
