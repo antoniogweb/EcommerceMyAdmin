f6899766290d5c28aefdb691067c5f654e14b646
antonio <antoniog.web@gmail.com>
2023-03-29 17:10:16 +0200

    mail se login da social

--- Frontend/Application/Views/_/Elementi/Mail/mail_login_da_social.php
+++ Frontend/Application/Views/_/Elementi/Mail/mail_login_da_social.php
@@ -1,5 +1,5 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 
-<p><?php echo gtext("Gentile cliente, hai effetuato il login sul sito utilizzando il tuo account",false);?> <?php echo $recordLoginApp["titolo"];?></p>
+<p><?php echo gtext("Gentile cliente, hai effetuato il login sul sito utilizzando il tuo account",false);?> <?php echo $recordLoginApp["titolo"]." ".$clean["username"];?></p>
 
 <p><?php echo gtext("Cordiali saluti", false);?>.</p>
