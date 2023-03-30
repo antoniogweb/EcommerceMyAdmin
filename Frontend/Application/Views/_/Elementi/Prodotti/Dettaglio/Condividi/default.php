a73ebff54396a43147067c4ca58b2dbce1bc29c1
antonio <antoniog.web@gmail.com>
2023-03-30 15:02:43 +0200

    label

--- Frontend/Application/Views/_/Elementi/Prodotti/Dettaglio/Condividi/default.php
+++ Frontend/Application/Views/_/Elementi/Prodotti/Dettaglio/Condividi/default.php
@@ -1,6 +1,6 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 <?php echo gtext("Condividi")?>:
-<a class="bo-social-facebook" href="http://www.facebook.com/sharer.php?u=<?php echo $this->baseUrl."/$urlAlias";?>&title=<?php echo field($p, "title");?>" target="_blank" title="<?php echo gtext("Condividi su facebook");?>">
+<a class="bo-social-facebook" href="http://www.facebook.com/sharer.php?u=<?php echo $this->baseUrl."/$urlAlias";?>&title=<?php echo field($p, "title");?>" target="_blank" title="<?php echo gtext("Condividi su Facebook");?>">
 	<span uk-icon="icon: facebook"></span>
 </a>
 <a class="bo-social-twitter" href="https://twitter.com/share?url=<?php echo $this->baseUrl."/$urlAlias";?>&amp;text=<?php echo field($p, "title");?>" target="_blank" title="<?php echo gtext("Condividi su Twitter");?>">
