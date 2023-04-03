a5cce6abcc3e8701b7dff77663ae4c55252a2341
antonio <antoniog.web@gmail.com>
2023-04-03 21:28:51 +0200

    collation

---
+++ DB/Migrazioni/1033.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `regioni` CHANGE `alias` `alias` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
---
+++ DB/Migrazioni/1034.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `fasce_prezzo` CHANGE `alias` `alias` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
