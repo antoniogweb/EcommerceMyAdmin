3ef9dec325eb5edd6a7822e841104195a3707cd0
antonio <antoniog.web@gmail.com>
2023-04-17 00:20:52 +0200

    antispam

--- Application/Controllers/Public/BaseOrdiniController.php
+++ Application/Controllers/Public/BaseOrdiniController.php
@@ -1412,7 +1412,7 @@ class BaseOrdiniController extends BaseController
 			}
 			else
 			{
-				$data['notice'] = "<div class='".v("alert_error_class")."'>".gtext("Sembra che ci sia un problema con la verifica dell'antispam.")."<br />".gtext("Assicurarsi di avere Javascript attivo nel browser.")."<br />".gtext("Provare inoltre a non utilizzare la compilazione automatica suggerita dal browser.")."<br />".gtext("Nel caso stiate utilizzando un dispositivo Apple (iPhone/iPad/Mac) con sistema operativo iOS 16, disabilitare la verifica automatica dei CAPTCHA nelle impostazioni.")."<br />".gtext("Nel caso la problematica continuasse contattare il negozio al")." <b>".v("telefono_aziendale")."</b> ".gtext("o tramite l'indirizzo email")." <b>".v("email_aziendale")."</b></div>".$this->m('OrdiniModel')->notice;
+				$data['notice'] = "<div class='".v("alert_error_class")."'>".gtext("Sembra che ci sia un problema con la verifica dell'antispam (CAPTCHA).")."<br />".gtext("Assicuratevi di avere Javascript attivo nel browser.")."<br />".gtext("Provate inoltre a non utilizzare la compilazione automatica suggerita dal browser.")."<br />".gtext("Nel caso stiate utilizzando un dispositivo Apple (iPhone/iPad/Mac) con sistema operativo iOS 16, disabilitate la verifica automatica dei CAPTCHA nelle impostazioni.")."<br />".gtext("Nel caso la problematica continuasse contattate il negozio al")." <b>".v("telefono_aziendale")."</b> ".gtext("o tramite l'indirizzo email")." <b>".v("email_aziendale")."</b></div>".$this->m('OrdiniModel')->notice;
 				$this->m('RegusersModel')->result = false;
 				$this->m('OrdiniModel')->result = false;
 				
