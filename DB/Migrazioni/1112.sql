9018f4dac6d17d0aea4ab96fa6cf3dc1368d7161
antonio <antoniog.web@gmail.com>
2023-09-21 15:57:19 +0200

    gestione accesso alle spedizioni in admin

---
+++ DB/Migrazioni/1112.sql
@@ -0,0 +1,1 @@
+INSERT INTO `controllers` (`id_controller`, `titolo`, `codice`, `codice_padre`, `visibile`, `condizioni`, `id_order`, `attivo`, `pannello`, `action_per_link`) VALUES (NULL, 'Spedizioni negozio - gestione righe', 'spedizioninegoziorighe', 'spedizioninegozio', '1', '', '91', '1', 'ecommerce', 'main')
