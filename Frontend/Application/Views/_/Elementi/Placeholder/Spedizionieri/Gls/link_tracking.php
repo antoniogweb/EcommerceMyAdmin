6d669598134aafc61de33b29f2f47382f7aaf2e1
antonio <antoniog.web@gmail.com>
2023-10-02 18:38:19 +0200

    link tracking su mail

--- Frontend/Application/Views/_/Elementi/Placeholder/Spedizionieri/Gls/link_tracking.php
+++ Frontend/Application/Views/_/Elementi/Placeholder/Spedizionieri/Gls/link_tracking.php
@@ -2,4 +2,6 @@
 
 <p>
 	<?php echo gtext("Ecco il link per vedere il tracking della tua spedizione");?>: <a href="<?php echo $modulo->getUrlTracking($idSpedizione)?>"><?php echo gtext("vai al tracking");?></a>
+	<br />
+	<?php echo gtext("Il tracking della spedizione potrebbe essere disponibile da domani.")?>
 </p>
