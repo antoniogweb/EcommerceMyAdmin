8bd76ea88a55dc7ba368eb79269a058265e329c3
antonio <antoniog.web@gmail.com>
2023-10-09 17:05:31 +0200

    menu loggato

--- Frontend/Application/Views/_/Elementi/header_user_box.php
+++ Frontend/Application/Views/_/Elementi/header_user_box.php
@@ -5,64 +5,12 @@ if (!isset($ukdropdown))
 ?>
 <div class="uk-padding-small uk-margin-remove uk-dropdown" uk-dropdown="<?php echo $ukdropdown;?>" style="min-width: 250px;">
 	<?php if ($islogged) { ?>
-	<ul class="uk-nav uk-dropdown-nav">
-		<li>
-			<a href="<?php echo $this->baseUrl."/area-riservata";?>" title="<?php echo gtext("Area riservata", false);?>"><?php echo gtext("Area riservata");?></a>
-		</li>
-		<?php if (v("attiva_biblioteca_documenti")) { ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/biblioteca-documenti/";?>" title="<?php echo gtext("Biblioteca documenti", false);?>"><?php echo gtext("Biblioteca documenti");?></a>
-		</li>
-		<?php } ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/ordini-effettuati";?>" title="<?php echo gtext("Ordini effettuati", false);?>"><?php echo gtext("Ordini effettuati");?></a>
-		</li>
-		<?php if (v("attiva_agenti") && User::$isAgente) { ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/promozioni/elenco/";?>" title="<?php echo gtext("Codici coupon", false);?>"><?php echo gtext("Codici coupon");?></a>
-		</li>
-		<li>
-			<a href="<?php echo $this->baseUrl."/ordini-collegati/";?>" title="<?php echo gtext("Ordini collegati", false);?>"><?php echo gtext("Ordini collegati");?></a>
-		</li>
-		<?php } ?>
-		<?php if (v("attiva_liste_regalo")) { ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/liste-regalo/";?>" title="<?php echo gtext("Liste nascita / regalo", false);?>"><?php echo gtext("Liste nascita / regalo");?></a>
-		</li>
-		<?php } ?>
-		<?php
-		if (v("abilita_feedback") && v("feedback_visualizza_in_area_riservata")) {
-			$user_feedback = FeedbackModel::get(0,0);
-			if (count($user_feedback) > 0) { ?>
-			<li>
-				<a href="<?php echo $this->baseUrl."/riservata/feedback";?>" title="<?php echo gtext("Le mie valutazioni", false);?>"><?php echo gtext("Le mie valutazioni");?></a>
-			</li>
-			<?php } ?>
-		<?php } ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/modifica-account";?>" title="<?php echo gtext("I miei dati", false);?>"><?php echo gtext("I miei dati");?></a>
-		</li>
-		<?php if (v("attiva_spedizione_area_riservata")) { ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/riservata/indirizzi";?>" title="<?php echo gtext("Spedizione", false);?>"><?php echo gtext("Spedizione");?></a>
-		</li>
-		<?php } ?>
-		<?php if (v("attiva_gestione_immagine_utente")) { ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/immagine-profilo";?>" title="<?php echo gtext("Immagine profilo", false);?>"><?php echo gtext("Immagine profilo");?></a>
-		</li>
-		<?php } ?>
-		<li>
-			<a href="<?php echo $this->baseUrl."/modifica-password";?>" title="<?php echo gtext("Modifica password", false);?>"><?php echo gtext("Modifica password");?></a>
-		</li>
-		<li>
-			<a href="<?php echo $this->baseUrl."/riservata/privacy";?>" title="<?php echo gtext("Gestione della privacy", false);?>"><?php echo gtext("Gestione della privacy");?></a>
-		</li>
-		<li class="uk-nav-divider"></li>
-		<li>
-			<a href="<?php echo $this->baseUrl."/esci";?>" title="<?php echo gtext("Esci", false);?>"><?php echo gtext("Esci");?></a>
-		</li>
-	</ul>
+	<?php
+	include(tpf(ElementitemaModel::p("HEADER_USER_BOX_LOGGED","", array(
+		"titolo"	=>	"Box dropdown utente loggato",
+		"percorso"	=>	"Elementi/Generali/HeaderUserBoxLogged",
+	))));
+	?>
 	<?php } else { ?>
 	<div class="uk-dropdown-nav">
 		<div class="uk-text-small uk-text-right">
