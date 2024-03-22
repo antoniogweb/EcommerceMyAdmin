3171ac9f1a3a8643d7667dcfe2b833096e7d6488
antonio <antoniog.web@gmail.com>
2024-03-22 11:24:26 +0100

    ticket

--- Frontend/Application/Views/_/Ticket/dettaglio.php
+++ Frontend/Application/Views/_/Ticket/dettaglio.php
@@ -3,27 +3,28 @@
 	<div class="first_of_grid tr_ragione_sociale uk-margin uk-margin-remove-bottom">
 		<div class="uk-text-primary uk-text-bold uk-margin-bottom-small"><?php echo gtext("Dettaglio della richiesta di assistenza");?></div>
 		
-		<div class="uk-width-1-1 uk-text-small">
-			<?php echo gtext("Tipologia della richiesta");?>: <b><?php echo $tipologia["titolo"];?></b><br />
-			<?php if ($ticket["id_o"]) { ?>
-			<?php echo gtext("N° Ordine");?>: <b>#<?php echo $ticket["id_o"];?></b><br />
-			<?php } ?>
-			<?php if ($ticket["id_lista_regalo"]) { ?>
-			<?php echo gtext("Lista regalo");?>: <?php echo TicketModel::getLabelLista($ticket["id_lista_regalo"]);?><br />
-			<?php } ?>
-			<br />
-			<span class="uk-text-bold"><?php echo gtext("Oggetto della richiesta");?>:</span><br />
-			<div class="uk-text-italic uk-text-secondary">
-				<?php echo $ticket["oggetto"];?>
+		<div class="uk-width-1-1">
+			<div class="uk-text-small">
+				<?php echo gtext("Tipologia della richiesta");?>: <b><?php echo $tipologia["titolo"];?></b><br />
+				<?php if ($ticket["id_o"]) { ?>
+				<?php echo gtext("N° Ordine");?>: <b>#<?php echo $ticket["id_o"];?></b><br />
+				<?php } ?>
+				<?php if ($ticket["id_lista_regalo"]) { ?>
+				<?php echo gtext("Lista regalo");?>: <?php echo TicketModel::getLabelLista($ticket["id_lista_regalo"]);?><br />
+				<?php } ?>
+				<br />
+				<span class="uk-text-bold"><?php echo gtext("Oggetto della richiesta");?>:</span><br />
+				<div class="uk-text-italic uk-text-secondary">
+					<?php echo $ticket["oggetto"];?>
+				</div>
+				<span class="uk-text-bold"><?php echo gtext("Descrizione");?>:</span>
+				
+				<div class="uk-text-italic">
+					<?php echo $ticket["descrizione"];?>
+				</div>
+				<?php $nominativoCliente = TicketModel::getNominativo($cliente);?>
+				<div class="uk-text-muted uk-text-small uk-margin"><?php echo gtext("Scritto da") . " <span class='uk-text-secondary'>". $nominativoCliente;?></span> <?php echo gtext("in data")?> <span class='uk-text-secondary'><?php echo date("d-m-Y H:i", strtotime($ticket["data_invio"]));?></span></div>
 			</div>
-			<span class="uk-text-bold"><?php echo gtext("Descrizione");?>:</span>
-			
-			<div class="uk-text-italic">
-				<?php echo $ticket["descrizione"];?>
-			</div>
-			<?php $nominativoCliente = TicketModel::getNominativo($cliente);?>
-			<div class="uk-text-muted uk-text-small uk-margin"><?php echo gtext("Scritto da") . " <span class='uk-text-secondary'>". $nominativoCliente;?></span> <?php echo gtext("in data")?> <span class='uk-text-secondary'><?php echo date("d-m-Y H:i", strtotime($ticket["data_invio"]));?></span></div>
-			
 			<?php if (!User::$isMobile) { ?>
 				<?php include(tpf("Ticket/messaggi.php")); ?>
 				<hr />
