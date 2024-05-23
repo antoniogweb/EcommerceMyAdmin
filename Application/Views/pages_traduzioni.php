d0b45f002017639620e6d1bdd8084dbeb1f58cd5
antonio <antoniog.web@gmail.com>
2024-05-23 17:15:32 +0200

    link traduzioni

--- Application/Views/pages_traduzioni.php
+++ Application/Views/pages_traduzioni.php
@@ -39,7 +39,7 @@ $cmT = new CategoriesModel();
 			else
 				$urlT = $cmT->getUrlAlias($trad["id_c"], $trad["lingua"]);
 			?>
-			<a target="_blank" title="Vedi pagina" href="<?php echo Domain::$name."/".$trad["lingua"].$nazioneUrl."/".$urlT;?>"><i class="fa fa-arrow-right"></i></a>
+			<?php include($this->viewPath("pages_traduzioni_link"));?>
 		</td>
 		<?php } ?>
 	</tr>
---
+++ Application/Views/pages_traduzioni_link.php
@@ -0,0 +1,2 @@
+<?php if (!defined('EG')) die('Direct access not allowed!'); ?>
+<a target="_blank" title="<?php echo gtext("Vedi pagina");?>" href="<?php echo Domain::$name."/".$trad["lingua"].$nazioneUrl."/".$urlT;?>"><i class="fa fa-arrow-right"></i></a>
