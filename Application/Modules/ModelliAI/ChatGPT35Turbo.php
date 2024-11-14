8e95c059e884d4d191b3b7fd58c92a5e7987c32b
Antonio Gallo <antoniog.web@gmail.com>
2024-11-14 11:28:40 +0100

    chatgpt

--- Application/Modules/ModelliAI/ChatGPT35Turbo.php
+++ Application/Modules/ModelliAI/ChatGPT35Turbo.php
@@ -87,7 +87,7 @@ class ChatGPT35Turbo extends ModelloAI
 
 				$responseArray = $response->toArray();
 
-				var_dump($responseArray);
+				// var_dump($responseArray);
 
 				if (isset($responseArray["choices"]) && is_array($responseArray["choices"]) && count($responseArray["choices"]) > 0)
 					return array(1, $responseArray["choices"][0]["message"]["content"]);
