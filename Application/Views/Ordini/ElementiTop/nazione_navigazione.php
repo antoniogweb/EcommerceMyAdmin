deeaddf9e7fd629edc2f586f629b4bbe249424ea
Antonio Gallo <antoniog.web@gmail.com>
2024-12-31 09:38:14 +0100

    dettaglio spedizione top ordine admin

--- Application/Views/Ordini/ElementiTop/nazione_navigazione.php
+++ Application/Views/Ordini/ElementiTop/nazione_navigazione.php
@@ -1,7 +1,7 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
-<?php if ($ordine["nazione_navigazione"] != v("nazione_default")) { ?>
+<?php if ($ordine["lingua"] != v("lingua_default_frontend") || $ordine["nazione_navigazione"] != v("nazione_default") || $ordine["nazione_spedizione"] != v("nazione_default")) { ?>
 <tr>
-	<td><?php echo v("attiva_ip_location") ? gtext("Lingua / Nazione navigazione") : gtext("Lingua / Nazione URL");?>:</td>
-	<td><b><?php echo LingueModel::getTitoloDaCodice($ordine["lingua"])." / ".findTitoloDaCodice($ordine["nazione_navigazione"]);?></b></td>
+	<td><?php echo v("attiva_ip_location") ? gtext("Lingua / Nazione navigazione / Nazione spedizione") : gtext("Lingua / Nazione URL / Nazione spedizione");?>:</td>
+	<td><b><?php echo LingueModel::getTitoloDaCodice($ordine["lingua"])." / ".findTitoloDaCodice($ordine["nazione_navigazione"])." / ".findTitoloDaCodice($ordine["nazione_spedizione"]);?></b></td>
 </tr>
 <?php } ?>
