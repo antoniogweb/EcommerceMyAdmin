12c2c9d844cd3f1a7a30a740a7886e068350c95e
Antonio Gallo <antoniog.web@gmail.com>
2025-01-15 15:24:43 +0100

    informativa cookie

--- Frontend/Application/Views/_/Elementi/Cookie/gestione_cookie.php
+++ Frontend/Application/Views/_/Elementi/Cookie/gestione_cookie.php
@@ -5,9 +5,10 @@
 <?php echo t("L'utente può gestire i cookie utilizzando le impostazioni del proprio browser.")?><br />
 <?php echo t("La disabilitazione totale o parziale dei cookie tecnici potrebbe compromettere l'utilizzo ottimale del sito.")?><br />
 <?php echo t("La disabilitazione dei cookie di terze parti non pregiudica in alcun modo la navigabilità.")?><br />
-<?php echo t("I browser la gestione dei gestire cookie per i diversi siti web. Di seguito i link sulla documentazione relativa alla gestione dei cookie per i diversi browser.")?><br />
+<?php echo t("I browser permettono la gestione dei cookie per i diversi siti web.")?><br />
+<?php echo t("Di seguito i link alla documentazione relativa alla gestione dei cookie per i diversi browser.")?><br />
 
-<ul>
+<ul class="uk-list uk-list-circle uk-list-collapse">
 	<li>Chrome: <a target="_blank" rel="nofollow" href="<?php echo t("https://support.google.com/chrome/answer/95647?hl=it");?>"><?php echo t("https://support.google.com/chrome/answer/95647?hl=it");?></a></li>
 	<li>Firefox: <a target="_blank" rel="nofollow" href="<?php echo t("https://support.mozilla.org/it/kb/Gestione%20dei%20cookie");?>"><?php echo t("https://support.mozilla.org/it/kb/Gestione%20dei%20cookie");?></a></li>
 	<li>Edge: <a target="_blank" rel="nofollow" href="<?php echo t("https://support.microsoft.com/it-it/windows/gestire-i-cookie-in-microsoft-edge-visualizzare-consentire-bloccare-eliminare-e-usare-168dab11-0753-043d-7c16-ede5947fc64d");?>"><?php echo t("https://support.microsoft.com/it-it/windows/gestire-i-cookie-in-microsoft-edge-visualizzare-consentire-bloccare-eliminare-e-usare-168dab11-0753-043d-7c16-ede5947fc64d");?></a></li>
