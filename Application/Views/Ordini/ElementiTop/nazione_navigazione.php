2604eb46915b55471b093565274741086335c990
Antonio Gallo <antoniog.web@gmail.com>
2025-02-28 12:36:35 +0100

    indicazione nazione su ordine

--- Application/Views/Ordini/ElementiTop/nazione_navigazione.php
+++ Application/Views/Ordini/ElementiTop/nazione_navigazione.php
@@ -2,7 +2,7 @@
 <?php
 $nazioneRilevanteIva = OrdiniModel::getNazioneRilevanteIvaOrdine($ordine);
 
-if ($ordine["lingua"] != v("lingua_default_frontend") || $ordine["nazione_navigazione"] != v("nazione_default") || $nazioneRilevanteIva != v("nazione_default")) { ?>
+if ($ordine["lingua"] != v("lingua_default_frontend") || $ordine["nazione_navigazione"] != v("nazione_default") || ($nazioneRilevanteIva && $nazioneRilevanteIva != v("nazione_default"))) { ?>
 <tr>
 	<td><?php echo v("attiva_ip_location") ? gtext("Lingua / Nazione navigazione / Nazione spedizione") : gtext("Lingua / Nazione URL / Nazione spedizione");?>:</td>
 	<td><b><?php echo LingueModel::getTitoloDaCodice($ordine["lingua"])." / ".findTitoloDaCodice($ordine["nazione_navigazione"])." / ".findTitoloDaCodice($nazioneRilevanteIva);?></b></td>
