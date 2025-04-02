f3729df7e4b06faffdaf53fd2294dd417858b831
Antonio Gallo <antoniog.web@gmail.com>
2025-04-02 15:43:42 +0200

    satispay

--- Application/Modules/GatewayPagamento/Satispay.php
+++ Application/Modules/GatewayPagamento/Satispay.php
@@ -249,7 +249,7 @@ class Satispay
 				{
 					if ($pagamento->status == "ACCEPTED")
 						$result = true;
-					else if ($sessione["status"] == "PENDING")
+					else if ($pagamento->status == "PENDING")
 						$inProgress = true;
 				}
 			}
