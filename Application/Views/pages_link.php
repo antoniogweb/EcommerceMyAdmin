a2f3807b048607b6c06a8e1cd677cca479b2c592
Antonio Gallo <antoniog.web@gmail.com>
2025-09-30 12:05:06 +0200

    link edit frontend

--- Application/Views/pages_link.php
+++ Application/Views/pages_link.php
@@ -1,8 +1,8 @@
 <?php if (!defined('EG')) die('Direct access not allowed!'); ?>
 <?php $nazioneUrl = v("attiva_nazione_nell_url") ? "_".strtolower(v("nazione_default")) : "";?>
 <?php if (isset($urlPagina)) { ?>
-<a class="pull-right label label-info" target="_blank" href="<?php echo Domain::$name."/it$nazioneUrl/$urlPagina";?>"><?php echo gtext("Vedi pagina");?> <i class="fa fa-arrow-right"></i></a>
+<a class="pull-right label label-info" target="_blank" href="<?php echo Domain::$name."/".v("lingua_default_frontend")."$nazioneUrl/$urlPagina";?>"><?php echo gtext("Vedi pagina");?> <i class="fa fa-arrow-right"></i></a>
 <?php } ?>
 <?php if (isset($urlPaginaEditFrontend)) { ?>
-<a class="label label-success" target="_blank" href="<?php echo Domain::$name."/it$nazioneUrl/$urlPaginaEditFrontend";?>"><?php echo gtext("Edit pagina frontend");?> <i class="fa fa-pencil"></i></a>
+<a class="label label-success" target="_blank" href="<?php echo Domain::$name."/".v("lingua_default_frontend")."$nazioneUrl/$urlPaginaEditFrontend";?>"><?php echo gtext("Edit pagina frontend");?> <i class="fa fa-pencil"></i></a>
 <?php } ?>
