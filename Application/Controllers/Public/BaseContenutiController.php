2734f97fb2b65ad3e61c81b775f72559180ecf5c
Antonio Gallo <antoniog.web@gmail.com>
2025-12-17 14:09:57 +0100

    cookie

--- Application/Controllers/Public/BaseContenutiController.php
+++ Application/Controllers/Public/BaseContenutiController.php
@@ -527,6 +527,7 @@ class BaseContenutiController extends BaseController
 				$this->estraiDatiFiltri();
 				
 				VariabiliModel::checkCookieTerzeParti();
+				VariabiliModel::noCookieAlert();
 				User::$id = 0;
 				User::$cart_uid = 0;
 				User::$wishlist_uid = 0;
