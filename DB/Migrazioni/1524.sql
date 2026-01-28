34ff324af4817e16ab1aef5bf402ddfd60066f24
Antonio Gallo <antoniog.web@gmail.com>
2026-01-28 21:06:02 +0100

    controllers

--- DB/Migrazioni/1524.sql
+++ DB/Migrazioni/1524.sql
@@ -1,1 +1,2 @@
-INSERT INTO `controllers` (`titolo`, `codice`, `codice_padre`, `visibile`, `condizioni`, `id_order`, `attivo`, `pannello`, `action_per_link`) VALUES ('Gestione statistiche download', 'documentidownload', '', '0', 'attiva_sezione_download_documenti=1', '112', '1', 'sito', 'main');
+INSERT INTO `controllers` (`titolo`, `codice`, `codice_padre`, `visibile`, `condizioni`, `id_order`, `attivo`, `pannello`, `action_per_link`) VALUES ('Gestione gruppi amministratori', 'controllers', 'users', '1', '', '122', '1', 'utenti', 'main');
+
