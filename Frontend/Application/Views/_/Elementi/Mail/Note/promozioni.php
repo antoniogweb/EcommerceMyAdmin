5f37f10afde9d05d349c0b41efd27ae1516ae9a4
Antonio Gallo <antoniog.web@gmail.com>
2026-03-02 14:24:29 +0100

    nota

--- Frontend/Application/Views/_/Elementi/Mail/Note/promozioni.php
+++ Frontend/Application/Views/_/Elementi/Mail/Note/promozioni.php
@@ -1,6 +1,6 @@
 <?php echo gtext("Gentile"); ?> [NOME],
 
-<?php echo gtext("in data"); ?> <?php echo date("d/m/Y");?> <?php echo gtext("il codice codice coupon"); ?> [CODICE] <?php echo gtext("è stato usato per un valore di "); ?> [VALORE_USATO] euro.
+<?php echo gtext("in data"); ?> <?php echo date("d/m/Y");?> <?php echo gtext("il codice coupon"); ?> [CODICE] <?php echo gtext("è stato usato per un valore di "); ?> [VALORE_USATO] euro.
 
 <?php echo gtext("Il valore finale rimasto del codice coupon è pari a")?> [VALORE_RIMASTO] euro.
 
