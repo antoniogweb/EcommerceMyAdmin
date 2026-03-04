851901aa5b2084e5b1354da93846ff3b9cb0c4e6
Antonio Gallo <antoniog.web@gmail.com>
2026-03-04 12:50:20 +0100

    va

--- Frontend/Public/Js/assistente_virtuale.js
+++ Frontend/Public/Js/assistente_virtuale.js
@@ -1,3 +1,11 @@
+function scorriChatInBasso()
+{
+	$('html, body').animate({
+		scrollTop: $(document).height()
+	}, 500);
+	// $("html, body").scrollTop($(document).height());
+}
+
 function aggiornaChat()
 {
 	$.ajaxQueue({
@@ -8,18 +16,31 @@ function aggiornaChat()
 		success: function(content){
 
 			$(".chat_messages").html(content);
+			scorriChatInBasso();
 
 		}
 	});
 }
 
 $(document).ready(function(){
+	scorriChatInBasso();
 	
 	$( "body" ).on( "click", ".send_request_to_va", function(e) {
 		
 		e.preventDefault();
+
+		var bottone = $(this);
+		var loader = bottone.find(".send_request_to_va_loader");
+		var testo = bottone.find(".send_request_to_va_text");
 		
 		var messaggio = $(".request_message").val();
+
+		if (!messaggio.length || bottone.prop("disabled"))
+			return;
+
+		bottone.prop("disabled", true);
+		testo.addClass("uk-hidden");
+		loader.removeClass("uk-hidden");
 		
 		$.ajaxQueue({
 			url: baseUrl + "/virtual-assistant/request/",
@@ -32,8 +53,14 @@ $(document).ready(function(){
 			dataType: "html",
 			success: function(content){
 				
+				$(".request_message").val("");
 				aggiornaChat();
 				
+			},
+			complete: function() {
+				bottone.prop("disabled", false);
+				testo.removeClass("uk-hidden");
+				loader.addClass("uk-hidden");
 			}
 		});
 	});
