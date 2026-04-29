c8e1569012a9d0b4851b6150f939e3229762656f
Antonio Gallo <antoniog.web@gmail.com>
2026-04-29 12:03:01 +0200

    typo

--- Application/Views/Ordini/ElementiTop/stato.php
+++ Application/Views/Ordini/ElementiTop/stato.php
@@ -4,7 +4,7 @@
 	<td>
 		<b><span class="label label-<?php echo labelStatoOrdine($ordine["stato"]);?>"><?php echo statoOrdine($ordine["stato"]);?></span></b>
 		<?php if (v("attiva_log_cambio_stato") && OrdinistatiModel::numero((int)$ordine["id_o"])) { ?>
-		<a <?php if (partial()) { ?>target="_blank"<?php } ?> class="<?php if (!partial()) { ?>iframe<?php } ?> pull-right help_storico_stati" href="<?php echo $this->baseUrl."/ordini/storicostati/".$ordine["id_o"];?>?partial=Y"><i class="fa fa-history"></i> <?php echo gtext("Vedi storico cambio stati");?></a>
+		<a <?php if (partial()) { ?>target="_blank"<?php } ?> class="<?php if (!partial()) { ?>iframe<?php } ?> pull-right help_storico_stati" href="<?php echo $this->baseUrl."/ordini/storicostati/".$ordine["id_o"];?>?partial=Y"><i class="fa fa-history"></i> <?php echo gtext("Vedi storico cambio stato");?></a>
 		<?php } ?>
 	</td>
 </tr>
