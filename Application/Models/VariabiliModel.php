4adaca12f071dd28a26ea82510c7c4733f7db762
Antonio Gallo <antoniog.web@gmail.com>
2026-06-03 09:45:13 +0200

    acquisti

--- Application/Models/VariabiliModel.php
+++ Application/Models/VariabiliModel.php
@@ -947,8 +947,8 @@ class VariabiliModel extends GenericModel {
 		"carica_header_csp"		=>	0, // se impostato su 1, imposta l'header CSP
 		"carica_header_csp_report_only"		=>	1, // se impostato su 1, imposta l'header CSP con "Content-Security-Policy-Report-Only"
 		### ACQUISTI ##
-		"attiva_modulo_acquisti"	=>	1, // se impostato su 1, attiva la sezione acquisti
-		"mostra_modulo_acquisti"	=>	1, // se impostato su 1, mostra il link al modulo acquisti nel pannello (non lo attiva!!)
+		"attiva_modulo_acquisti"	=>	0, // se impostato su 1, attiva la sezione acquisti
+		"mostra_modulo_acquisti"	=>	0, // se impostato su 1, mostra il link al modulo acquisti nel pannello (non lo attiva!!)
 		###
 		"attiva_tag_in_testi"	=>	0, // se impostato a 0 sarà possibile selezionare il tag contenitore dell'elemento
 		"attiva_redirect"		=>	0, // se impostato a 1, permette di gestire i redirect
