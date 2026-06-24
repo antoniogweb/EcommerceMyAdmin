37402d9f167740b2519b79d302dbfe527e786e37
Antonio Gallo <antoniog.web@gmail.com>
2026-06-24 14:33:50 +0200

    testo

--- Frontend/Application/Views/_/Listeregalo/resi.php
+++ Frontend/Application/Views/_/Listeregalo/resi.php
@@ -9,7 +9,7 @@
 			<br />
 			<span class="uk-text-small uk-text-warning">
 				<?php echo gtext("In data")." <b>".smartDate($pr["data_richiesta"], v("default_date_format")." H:i");?></b>
-				<?php echo gtext("hai eseguito una richiesta di resto per i prodotti della lista consegnati il")." <b>".smartDate($pr["data_inizio"], v("default_date_format")); ?></b>
+				<?php echo gtext("hai eseguito una richiesta di reso per i prodotti della lista consegnati il")." <b>".smartDate($pr["data_inizio"], v("default_date_format")); ?></b>
 			</span>
 		<?php } else { ?>
 			<a target="_blank" class="uk-margin-left uk-button uk-button-primary uk-button-small" href="<?php echo OrdiniperiodiresoModel::g(false)->getUrlRichiediResoLista($pr["id_o_periodo_reso"])?>"><?php echo gtext("Richiedi il reso", false); ?> <span class="uk-icon"><?php include tpf("Elementi/Icone/Svg/arrow-right.svg");?></span></a><br />
