95e129871ef82df2d13dafe54538ccc197af3afb
Antonio Gallo <antoniog.web@gmail.com>
2026-09-09 16:26:42 +0200

    gestion npm

--- External/npm/scripts/vendor.js
+++ External/npm/scripts/vendor.js
@@ -1,27 +1,39 @@
 const fs = require('fs');
 
+const vendorPaths = {
+    jquery: '../../Public/Js/vendor/jquery/',
+    jqueryUi: '../../Public/Js/vendor/jquery-ui/',
+    dropzone: '../../Public/Js/vendor/dropzone/',
+    select2: '../../Public/Js/vendor/select2/',
+};
+
+for (const path of Object.values(vendorPaths)) {
+    fs.rmSync(path, { recursive: true, force: true });
+    fs.mkdirSync(path, { recursive: true });
+}
+
 // jQuery
 fs.copyFileSync(
     'node_modules/jquery/dist/jquery.min.js',
-    '../../Public/Js/vendor/jquery/jquery.min.js'
+    vendorPaths.jquery + 'jquery.min.js'
 ); 
+
+fs.copyFileSync(
+    'node_modules/jquery-migrate/dist/jquery-migrate.min.js',
+    vendorPaths.jquery + 'jquery-migrate-1.4.1.min.js'
+);
 
 // jQuery UI: JavaScript and widget structure are supplied by npm. The
 // ui-lightness ThemeRoller variant used by the application is kept under
 // assets, because the official package ships only its base theme.
-fs.rmSync(
-    '../../Public/Js/vendor/jquery-ui/images',
-    { recursive: true, force: true }
-);
-
 fs.cpSync(
     'assets/jquery-ui-theme/images',
-    '../../Public/Js/vendor/jquery-ui/images',
+    vendorPaths.jqueryUi + 'images',
     { recursive: true }
 );
 
 const jqueryUiSourcePath = 'node_modules/jquery-ui/';
-const destinationPath = '../../Public/Js/vendor/jquery-ui/';
+const destinationPath = vendorPaths.jqueryUi;
 
 fs.copyFileSync(
     jqueryUiSourcePath + 'dist/jquery-ui.min.js',
@@ -61,9 +73,7 @@ fs.writeFileSync(
 
 // Dropzone
 const dropzoneSourcePath = 'node_modules/dropzone/dist/min/';
-const dropzoneDestinationPath = '../../Public/Js/vendor/dropzone/';
-
-fs.mkdirSync(dropzoneDestinationPath, { recursive: true });
+const dropzoneDestinationPath = vendorPaths.dropzone;
 
 for (const file of ['dropzone.min.js', 'dropzone.min.css']) {
     fs.copyFileSync(
@@ -74,9 +84,7 @@ for (const file of ['dropzone.min.js', 'dropzone.min.css']) {
 
 // Select2
 const select2SourcePath = 'node_modules/select2/dist/';
-const select2DestinationPath = '../../Public/Js/vendor/select2/';
-
-fs.mkdirSync(select2DestinationPath, { recursive: true });
+const select2DestinationPath = vendorPaths.select2;
 
 fs.copyFileSync(
     select2SourcePath + 'js/select2.min.js',
